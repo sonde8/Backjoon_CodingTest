aba8fd1f3b7d1ff31aa39e5bc074637114d17dea
유승재 <sonde8@naver.com>
2026-02-05 16:29:39 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        String id2 = id;
+
+        if (id == id2) {
+          System.out.println(id2 + "??!");
+        };
+
+
+    }
+}
