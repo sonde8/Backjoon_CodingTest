f0029a706aae66613e7975d37c6a752ecf634259
유승재 <sonde8@naver.com>
2026-02-05 16:06:33 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int result = num1 - num2;
+
+        System.out.println(result);
+    }
+}
